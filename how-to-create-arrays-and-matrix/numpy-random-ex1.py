6cd7f26fe1a2eb63ea1584072f639bd026522c80
Myounis <muhammad.younis@stewart.com>
2025-03-14 14:15:45 +0500

    added-comments

--- how-to-create-arrays-and-matrix/numpy-random-ex1.py
+++ how-to-create-arrays-and-matrix/numpy-random-ex1.py
@@ -1,13 +1,30 @@
 from numpy import random
-# creating random matrix of data type float
-# of two rows and 3 colums
+# Creating random uniform distribution matrix of data type float
+# of two rows and three columns
 a1 = random.rand(2,3)
 print(a1)
+# printing shape of array
+print(a1.shape)
+# printing dtype of array
+print(a1.dtype)
+print("\n")
 
-# creating int type of random one to ten numbers matrix having four rows and columns
+# Creating int type of random one to ten numbers matrix 
+# having four rows and columns
 a2 = random.randint(1,10,(4,4))
 print(a2)
+# printing shape of array
+print(a2.shape)
+# printing dtype of array
+print(a2.dtype)
+print("\n")
 
-# creating 
+# Creating random normal distribution matrix of data type float
+# of two rows and three columns
 a3 = random.randn(2,3)
 print(a3)
+# printing shape of array
+print(a3.shape)
+# printing dtype of array
+print(a3.dtype)
+print("\n")
