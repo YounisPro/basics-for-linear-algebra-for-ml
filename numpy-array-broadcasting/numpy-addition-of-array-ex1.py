ac7a218d2565dc801e2b658fc35c6bee09168860
Muhammad Younis <younissabbasi@gmail.com>
2025-03-16 18:45:11 +0500

    added-chapter-six

---
+++ numpy-array-broadcasting/numpy-addition-of-array-ex1.py
@@ -0,0 +1,18 @@
+from numpy import array
+# creating data set
+a = [1, 2, 3]
+b = [1, 2, 3]
+# this will not working and instead it will output
+# [1, 2, 3, 1, 2, 3]
+c = a + b
+# output: [1, 2, 3, 1, 2, 3]
+print(c)
+print("\n")
+
+# creating arrays of above datasets
+arr1 = array(a)
+arr2 = array(b)
+# adding two arrays
+sum = arr1 + arr2
+# output
+print(sum)
