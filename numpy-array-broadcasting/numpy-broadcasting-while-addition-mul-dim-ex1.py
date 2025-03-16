52de3b900b176c845a4cee94960777f7a09a5427
Muhammad Younis <younissabbasi@gmail.com>
2025-03-16 22:57:24 +0500

    update-names-of-files

---
+++ numpy-array-broadcasting/numpy-broadcasting-while-addition-mul-dim-ex1.py
@@ -0,0 +1,16 @@
+from numpy import array
+# creating array
+arr = array([
+    [1, 2, 3],
+    [1, 2, 3]
+])
+# printing array
+print(arr)
+
+# define scalar/broadcaster variable
+b = 2
+print(b)
+
+# broadcasting/addition with matrix's each element
+sum = arr + b
+print(sum)
